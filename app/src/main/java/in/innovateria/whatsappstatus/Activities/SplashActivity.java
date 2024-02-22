309ff08bce35a7a01dbe95823e64aa1128911997
Vivek Kumar <vnjvibhash@gmail.com>
2024-02-22 19:41:33 +0530

    Initial Commit

---
+++ app/src/main/java/in/innovateria/whatsappstatus/Activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package in.innovateria.whatsappstatus.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.innovateria.whatsappstatus.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
